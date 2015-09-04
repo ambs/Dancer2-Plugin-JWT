157b7f72cfd1504dc59afa1912369c2ffc682b90
ambs <ambs@cpan.org>
2015-09-04 14:32:32 +0100

    First version

--- lib/Dancer2/Plugin/JWT.pm
+++ lib/Dancer2/Plugin/JWT.pm
@@ -89,3 +89,74 @@ on_plugin_import {
 register_plugin;
 
 1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dancer2::Plugin::JWT - JSON Web Token made simple for Dancer2
+
+=head1 SYNOPSIS
+
+     use Dancer2;
+     use Dancer2::Plugin::JWT;
+
+     post '/login' => sub {
+         if (is_valid(param("username"), param("password"))) {
+            jwt { username => param("username") };
+            template 'index';
+         }
+         else {
+             redirect '/';
+         }
+     };
+
+     get '/private' => sub {
+         my $data = jwt;
+         redirect '/ unless exists $data->{username};
+
+         ...
+     };
+
+=head1 DESCRIPTION
+
+Registers the C<jwt> keyword that can be used to set or retrieve the payload
+of a JSON Web Token.
+
+To this to work it is required to have a secret defined in your config.yml file:
+
+   plugins:
+      JWT:
+          secret: "my little secret"
+
+
+=head1 BUGS
+
+I am sure a lot. Please use GitHub issue tracker 
+L<here|https://github.com/ambs/Dancer2-Plugin-JWT/>.
+
+=head1 ACKNOWLEDGEMENTS
+
+To Lee Johnson for his talk "JWT JWT JWT" in YAPC::EU::2015.
+
+To Yuji Shimada for JSON::WebToken.
+
+To Nuno Carvalho for brainstorming and help with testing.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2015 Alberto Simões, all rights reserved.
+
+This module is free software and is published under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Alberto Simões C<< <ambs@cpan.org> >>
+
+=cut
+
+
+
+
+
+
