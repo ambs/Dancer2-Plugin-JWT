97cd01d6e3d5eb376f7561cb58b4926b3a6ad0dd
Mikko Koivunalho <mikkoi@cpan.org>
2022-09-25 22:51:23 +0200

    Correct operator mismatch
    The variable $need_key is numeric.
    Its only possible values are undef, 1 and 2.

--- lib/Dancer2/Plugin/JWT.pm
+++ lib/Dancer2/Plugin/JWT.pm
@@ -131,7 +131,7 @@ on_plugin_import {
         }
 
         if ( defined $need_key ) {
-            if ( $need_key eq 1 ) {
+            if ( $need_key == 1 ) {
                 # TODO: add code to handle RSA keys or parse JWK hash string:
                 ##instance of Crypt::PK::RSA
                 #my $data = decode_jwt(token=>$t, key=>Crypt::PK::RSA->new('keyfile.pem'));
@@ -141,7 +141,7 @@ on_plugin_import {
                 #
                 ##instance of Crypt::OpenSSL::X509 (public key only)
                 #my $data = decode_jwt(token=>$t, key=>Crypt::OpenSSL::X509->new_from_file('cert.pem'));
-            } elsif ( $need_key eq 2 ) {
+            } elsif ( $need_key == 2 ) {
                 # TODO: add code to handle ECC keys or parse JWK hash string:
                 #instance of Crypt::PK::ECC
                 #my $data = decode_jwt(token=>$t, key=>Crypt::PK::ECC->new('keyfile.pem'));
